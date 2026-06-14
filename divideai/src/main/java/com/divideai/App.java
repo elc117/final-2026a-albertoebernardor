144282df2dc7990a0a1de8ecc50781035aba6aaf
albertoalves34 <155261962+albertoalves34@users.noreply.github.com>
2026-06-14 19:08:22 -0300

    bugfix: alterando versão e App.java

--- divideai/src/main/java/com/divideai/App.java
+++ divideai/src/main/java/com/divideai/App.java
@@ -1,17 +1,20 @@
 package com.divideai;
 
 import io.javalin.Javalin;
+import io.javalin.http.Handler;
 
 public class App {
     public static void main(String[] args) {
         int port = Integer.parseInt(System.getenv().getOrDefault("PORT", "8080"));
 
-        Javalin app = Javalin.create(config -> {
+        Handler teste = ctx -> ctx.result("ok");
+
+        Javalin.create(config -> {
             config.bundledPlugins.enableCors(cors -> {
                 cors.addRule(rule -> rule.anyHost());
             });
-        }).start(port);
-
-        app.get("/health", ctx -> ctx.result("ok"));
+        })
+        .get("/teste", teste)
+        .start(port);
     }
 }
