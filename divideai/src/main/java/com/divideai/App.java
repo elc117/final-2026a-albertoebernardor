853a367dd629bf31a972475b206bd407350c2c56
albertoalves34 <155261962+albertoalves34@users.noreply.github.com>
2026-06-23 19:42:08 -0300

    feature: Adicionando inicialização de rotas

--- divideai/src/main/java/com/divideai/App.java
+++ divideai/src/main/java/com/divideai/App.java
@@ -1,20 +1,36 @@
 package com.divideai;
 
+import com.divideai.controller.GrupoController;
+import com.divideai.controller.UsuarioController;
+import com.divideai.repository.GrupoRepository;
+import com.divideai.repository.UsuarioRepository;
+import com.divideai.service.GrupoService;
+import com.divideai.service.UsuarioService;
+
 import io.javalin.Javalin;
-import io.javalin.http.Handler;
 
 public class App {
     public static void main(String[] args) {
         int port = Integer.parseInt(System.getenv().getOrDefault("PORT", "8080"));
 
-        Handler teste = ctx -> ctx.result("ok");
+        UsuarioRepository usuarioRepository = new UsuarioRepository();
+        GrupoRepository grupoRepository = new GrupoRepository();
 
-        Javalin.create(config -> {
+        UsuarioService usuarioService = new UsuarioService(usuarioRepository);
+        GrupoService grupoService = new GrupoService(grupoRepository);
+
+        UsuarioController usuarioController = new UsuarioController(usuarioService);
+        GrupoController grupoController = new GrupoController(grupoService);
+
+        Javalin app = Javalin.create(config -> {
             config.bundledPlugins.enableCors(cors -> {
                 cors.addRule(rule -> rule.anyHost());
             });
-        })
-        .get("/teste", teste)
-        .start(port);
+        });
+
+        usuarioController.registrarRotas(app);
+        grupoController.registarRotas(app);
+
+        app.start(port);
     }
 }
