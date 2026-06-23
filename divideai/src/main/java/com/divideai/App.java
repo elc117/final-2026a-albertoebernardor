0af517212ae4d827c7b6bb87a55e05bc747b43bc
albertoalves34 <155261962+albertoalves34@users.noreply.github.com>
2026-06-23 20:33:55 -0300

    bugfix: Adicionando dependencia no pom

--- divideai/src/main/java/com/divideai/App.java
+++ divideai/src/main/java/com/divideai/App.java
@@ -6,8 +6,12 @@ import com.divideai.repository.GrupoRepository;
 import com.divideai.repository.UsuarioRepository;
 import com.divideai.service.GrupoService;
 import com.divideai.service.UsuarioService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import io.javalin.Javalin;
+import io.javalin.json.JavalinJackson;
 
 public class App {
     public static void main(String[] args) {
@@ -22,10 +26,15 @@ public class App {
         UsuarioController usuarioController = new UsuarioController(usuarioService);
         GrupoController grupoController = new GrupoController(grupoService);
 
+        ObjectMapper mapper = new ObjectMapper()
+                .registerModule(new JavaTimeModule())
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
         Javalin app = Javalin.create(config -> {
             config.bundledPlugins.enableCors(cors -> {
                 cors.addRule(rule -> rule.anyHost());
             });
+            config.jsonMapper(new JavalinJackson(mapper, false));
         });
 
         usuarioController.registrarRotas(app);
